6f99f2d34d26d6dd6568764479cb1427d16c1f9f
Siong <siong1987@gmail.com>
2020-02-20 15:07:41 +0800

    some minor changes.

--- dummy.sol
+++ dummy.sol
@@ -32,14 +32,10 @@ contract Token {
     function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-    
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);    
 }
 
-
-
 contract StandardToken is Token {
-
     function transfer(address _to, uint256 _value) returns (bool success) {
         //Default assumes totalSupply can't be over max (2^256 - 1).
         //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
@@ -84,10 +80,8 @@ contract StandardToken is Token {
     uint256 public totalSupply;
 }
 
-
 //name this contract whatever you'd like
 contract ERC20Token is StandardToken {
-
     function () {
         //if ether is sent to this address, send it back.
         throw;
@@ -105,12 +99,6 @@ contract ERC20Token is StandardToken {
     uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
     string public symbol;                 //An identifier: eg SBX
     string public version = 'H1.0';       //human 0.1 standard. Just an arbitrary versioning scheme.
-
-//
-// CHANGE THESE VALUES FOR YOUR TOKEN
-//
-
-//make sure this function name matches the contract name above. So if you're token is called TutorialToken, make sure the //contract name above is also TutorialToken instead of ERC20Token
 
     function ERC20Token(
         ) {
